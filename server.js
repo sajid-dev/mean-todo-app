e6a3c8a00528324f2e04b7a144d2ea5039c4f483
Sajid <sajid.dev@outlook.com>
2017-08-11 12:06:38 -0500

    Added angular client and changed index route and server.js updated

--- server.js
+++ server.js
@@ -16,12 +16,17 @@ var app = express();
 /* View Engine */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, 'client/src')));
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
         extended: false
     }
 ));
+
 
 app.use('/', index);
 app.use('/api/v1/', todos);
